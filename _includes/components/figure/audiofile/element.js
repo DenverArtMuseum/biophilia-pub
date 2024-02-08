0dd7ed8f278deb72dadf94a4d0298551721e90f9
Matt Popke <mpopke@denverartmuseum.org>
2024-02-08 14:14:45 -0700

    Adding audio file figure type and html/javascript controller.

--- _includes/components/figure/audiofile/element.js
+++ _includes/components/figure/audiofile/element.js
@@ -23,24 +23,26 @@ module.exports = function(eleventyConfig) {
       : path.join(imageDir,audio_src)
 
     return html`<div id="audio-player-${ id }" class="audio-player-container">
-      <audio id="audio-player-file-${ id }" class="audio-file" src="${ audioSrc }" preload="metadata"></audio>
-      <div class="title-block">
-        <p id="audio-player-title-${ id }" class="track-title">
-          ${ title }
-        </p>
-      </div>
-      <div class="play-block">
-        <button id="audio-player-play-button-${ id }" class="play-pause-button paused">Play</button>
-        <span id="audio-player-current-time-${ id }" class="time current-time">0:00</span>
-        <input type="range" id="audio-player-seek-slider-${ id }" class="seek-slider" max="100" value="0">
-        <span id="audio-player-duration-${ id }" class="time duration">0:00</span>
-      </div>
-      <div class="volume-block">
-        <button id="audio-player-mute-player-${ id }" class="mute-button unmuted">Mute</button>
-        <input type="range" id="audio-player-volume-slider-${ id }" class="volume-slider" max="100" value="100">
-        <output id="audio-player-volume-output-${ id }" class="volume-output">100</output>
-      </div>
-  </div>`
+  <audio id="audio-player-file-${ id }" class="audio-file" src="${ audioSrc }" preload="metadata"></audio>
+  <div class="title-block">
+    <p id="audio-player-title-${ id }" class="track-title">
+      ${ title }
+    </p>
+  </div>
+  <div class="control-block">
+    <div class="play-block">
+      <button id="audio-player-play-button-${ id }" class="play-pause-button paused">Play</button>
+      <span id="audio-player-current-time-${ id }" class="time current-time">0:00</span>
+      <input type="range" id="audio-player-seek-slider-${ id }" class="seek-slider" max="100" value="0">
+      <span id="audio-player-duration-${ id }" class="time duration">0:00</span>
+    </div>
+    <div class="volume-block">
+      <button id="audio-player-mute-player-${ id }" class="mute-button unmuted">Mute</button>
+      <input type="range" id="audio-player-volume-slider-${ id }" class="volume-slider" max="100" value="100">
+      <output id="audio-player-volume-output-${ id }" class="volume-output">100</output>
+    </div>
+  </div>
+</div>`
 
   }
 }
